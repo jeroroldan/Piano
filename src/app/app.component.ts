3219847620b22ed155253a093d9864ea197310c4
jeronimo <jeronimoroldan@outlook.com.ar>
2021-02-12 17:40:39 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano';
+
+  aplicarSound(numero: number): void {
+
+    const audio = new Audio();
+
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+
+
+
+
+  }
+
+
+
+}
